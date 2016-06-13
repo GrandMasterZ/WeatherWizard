3ce42df3b7c63080dad2fcb982d6e7ba4c4e3a21
Mantas Zambacevicius <zambacevicius.mantas@gmail.com>
2016-06-13 19:16:51 +0300

    Added react component that loads data from api

---
+++ js/App.jsx
@@ -0,0 +1,79 @@
+import TimerMixin from 'react-timer-mixin';
+const React = require('react')
+const ReactDOM = require('react-dom')
+const $ = require('jquery')
+require('./map.js')
+
+var ReportUI = React.createClass({
+
+    mixins:[TimerMixin],
+    getInitialState:function()
+    {
+        return {
+            weather: {},
+            location: {}
+        }
+    },
+
+    loadWeatherByCity: function(city)
+    {
+        var url = 'http://api.apixu.com/v1/current.json?key=a76117fc3c5841c7b5c152812161206&q='+city;
+        var _this = this;
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({location: data.location, weather: data.current});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }
+        });
+    },
+
+    loadWeatherByLocation: function()
+    {
+        var city;
+        var getIpUrl='http://ip-api.com/json';
+        $.ajax({
+            url: getIpUrl,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                city = data.city;
+                this.loadWeatherByCity(city);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    componentDidMount: function()
+    {
+        this.loadWeatherByLocation();
+        this.setInterval(() => {this.loadWeatherByLocation();}, 60000);
+    },
+
+    render: function() {
+        return (
+            <div className="reportBox">
+                <h2> {this.state.location.name} </h2>
+                <h2> {this.state.location.localtime} </h2>
+                <div class="row margin">
+                    <input className="col-xs-4 noPadding reportButton" type="button" value="Previous day"/>
+                    <input className="col-xs-4 noPadding reportButton" type="button" value="Next day"/>
+                    <input className="col-xs-4 noPadding reportButton" type="button" value="Calendar"/>
+                </div>
+                <p className="temp">{this.state.weather.feelslike_c} ℃ </p>
+                <p> Humidity {this.state.weather.humidity} </p>
+                <p> Wind direction {this.state.weather.wind_dir}</p>
+                <p> Wind speed {this.state.weather.wind_kph} </p>
+                <p> Last updated {this.state.weather.last_updated} </p>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<ReportUI/>, document.getElementById('reportUI'))
