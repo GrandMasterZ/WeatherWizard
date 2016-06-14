0af1784a98e91f7c43715215c6583aa1765f2e81
Mantas Zambacevicius <zambacevicius.mantas@gmail.com>
2016-06-14 17:55:21 +0300

    some changes

--- js/App.jsx
+++ js/App.jsx
@@ -14,19 +14,20 @@ var ReportUI = React.createClass({
             weather: {},
             location: {},
             info: {},
-            currentCity:{}
+            currentCity: {},
+            forecastDelivered: false,
+            forecast: []
         }
     },
-
+    
     loadWeatherByCity: function (city) {
-        var url = 'http://api.apixu.com/v1/current.json?key=a76117fc3c5841c7b5c152812161206&q=' + city;
-        var _this = this;
+        var url = 'http://api.apixu.com/v1/forecast.json?key=a76117fc3c5841c7b5c152812161206&q=' + city + '&days=10';
         $.ajax({
             url: url,
             dataType: 'json',
             cache: false,
             success: function (data) {
-                this.setState({location: data.location, weather: data.current, info: data.current.condition});
+                this.setState({location: data.location, weather: data.current, info: data.current.condition, forecast: data.forecast.forecastday});
             }.bind(this),
             error: function (xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -36,6 +37,7 @@ var ReportUI = React.createClass({
 
     searchByCity: function ()
     {
+        this.setState({forecastDelivered:false});
         var text = document.getElementById('cityText').value;
         this.loadWeatherByCity(text);
         this.setState({currentCity:text});
@@ -53,6 +55,7 @@ var ReportUI = React.createClass({
             success: function(data) {
                 city = data.city;
                 this.loadWeatherByCity(city);
+                this.setState({currentCity:city});
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -64,23 +67,29 @@ var ReportUI = React.createClass({
     {
         this.loadWeatherByLocation();
         this.setInterval(() => {this.loadWeatherByCity(this.state.currentCity);}, 60000);
-        $(document.body).on('keydown', this.fired);
+        $(document.body).on('keydown', this.submitEnter);
     },
 
-    fired:function(e)
+    submitEnter:function(e)
     {
         if (e.keyCode === 13) {
+            this.setState({forecastDelivered:false});
             var text = document.getElementById('cityText').value;
             this.loadWeatherByCity(text);
             this.setState({currentCity:text});
         }
     },
 
+    changeForecastStatus:function()
+    {
+        this.setState({forecastDelivered:true});
+    },
+    
     render: function() {
         return (
             <div>
                 <Form search={this.searchByCity} />
-                <Report weather={this.state.weather} location={this.state.location} info={this.state.info} />
+                <Report weather={this.state.weather} location={this.state.location} changeStatus={this.changeForecastStatus} status={this.state.forecastDelivered} info={this.state.info} />
             </div>
         );
     }
--- js/Report.jsx
+++ js/Report.jsx
@@ -1,21 +1,40 @@
+const { If, Then, Else } = require('react-if');
 const React = require('react')
 
 var Report = React.createClass({
+
     render: function() {
+        let reportContent;
+        if(this.props.status)
+        {
+            reportContent = (
+                <div className="reportBox">
+                    <p> Delivered forecast</p>
+                </div>
+            )
+        }
+        else
+        {
+            reportContent = (
+                <div className="reportBox">
+                    <h2> {this.props.location.name} </h2>
+                    <h2> {this.props.location.localtime} </h2>
+                    <div class="row margin">
+                        <input className="col-xs-4 noPadding reportButton" type="button" value="Previous day"/>
+                        <input onClick={this.test} className="col-xs-4 noPadding reportButton" type="button" value="Next day"/>
+                        <input className="col-xs-4 noPadding reportButton" type="button" value="Calendar"/>
+                    </div>
+                    <p className="temp">{this.props.info.text} {this.props.weather.feelslike_c}℃ <img src={'http:' + this.props.info.icon} /> </p>
+                    <p className="temp"> {this.props.weather.humidity} <img width="64" height="64" src="/home/asdsda/Desktop/WeatherWizard/images/Humidity1.png" /> </p>
+                    <p> Wind direction {this.props.weather.wind_dir}</p>
+                    <p> Wind speed {this.props.weather.wind_kph} kph</p>
+                    <p> Last updated: {this.props.weather.last_updated} </p>
+                </div>
+            )
+        }
         return (
             <div className="reportBox">
-                <h2> {this.props.location.name} </h2>
-                <h2> {this.props.location.localtime} </h2>
-                <div class="row margin">
-                    <input className="col-xs-4 noPadding reportButton" type="button" value="Previous day"/>
-                    <input className="col-xs-4 noPadding reportButton" type="button" value="Next day"/>
-                    <input className="col-xs-4 noPadding reportButton" type="button" value="Calendar"/>
-                </div>
-                <p className="temp">{this.props.info.text} {this.props.weather.feelslike_c}℃ <img src={'http:' + this.props.info.icon} /> </p>
-                <p className="temp"> {this.props.weather.humidity} <img width="64" height="64" src="/home/asdsda/Desktop/WeatherWizard/images/Humidity1.png" /> </p>
-                <p> Wind direction {this.props.weather.wind_dir}</p>
-                <p> Wind speed {this.props.weather.wind_kph} kph</p>
-                <p> Last updated: {this.props.weather.last_updated} </p>
+                {reportContent}
             </div>
         );
     }
